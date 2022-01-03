dd1e726638ff2679c8bcddf45248a4d5544c5043
Anthony <anthony.fedorenko@gmail.com>
2022-01-03 18:35:26 +0200

    Starred new project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+// Css
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
+import '../node_modules/magnific-popup/dist/magnific-popup.css';
+import './assets/css/plugins/animate.min.css';
+import './assets/fonts/flaticon/flaticon.css';
+import './assets/css/plugins/font-awesome.min.css';
+import './assets/css/style.css';
+import './assets/css/responsive.css';
+import './index.css';
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={"/themes/themeforest/react/sacreva/"}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('sacreva')
 );
 
 // If you want to start measuring performance in your app, pass a function
